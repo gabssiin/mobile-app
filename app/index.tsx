6ae60b17e7b6f87947bc923ca830a69214ce0612
gabssiin <gabriel.willian.br@gmail.com>
2025-08-21 11:51:38 -0300

    adicionando image

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, Image } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -7,12 +7,28 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#828d9bff",
+        
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Image>
-        
-      </Image>
+      
+      <Image source={
+      require('../assets/images/luto.jpg')}></Image>
+      <Text> Gabriel Santos</Text>
+      <Text style={{backgroundColor:"#FFF"}}>Minha bio é isso ai </Text>
     </View>
   );
 }
+
+
+/*melhor usar flutter
+escrever apenas view android usando kotlin ou java
+ios swift ou objectve c
+o código se adequa ao sistema que o usa
+
+Core Components:
+View, Text, Image, ScrollView, TextInput (principais)
+Importar tudo
+
+*/
+
