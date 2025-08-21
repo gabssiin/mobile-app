c1e9e488675f5ab1cefbfb41d9c7da735b0f34ac
gabssiin <gabriel.willian.br@gmail.com>
2025-08-21 12:49:27 -0300

    começando a atividaden1

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
@@ -7,19 +7,23 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#828d9bff",
-        
+        backgroundColor: "#aaaaaaff",
       }}
     >
-      
-      <Image source={
-      require('../assets/images/luto.jpg')}></Image>
-      <Text> Gabriel Santos</Text>
-      <Text style={{backgroundColor:"#FFF"}}>Minha bio é isso ai </Text>
+      <Image
+        style={{
+          width: 200,
+          height: 200,
+          margin: 50,
+          resizeMode: "contain",
+        }}
+        source={require("../assets/images/foto.jpg")}
+      ></Image>
+      <Text style={{ color: "fff" }}> Gabriel Santos.</Text>
+      <Text /*style={{backgroundColor:"#FFF"}}*/>Minha bio é isso ai </Text>
     </View>
   );
 }
-
 
 /*melhor usar flutter
 escrever apenas view android usando kotlin ou java
@@ -30,5 +34,15 @@ Core Components:
 View, Text, Image, ScrollView, TextInput (principais)
 Importar tudo
 
-*/
+Crie um App em React Native + Expo (utilizando o onboard hello) que tenha um componente chamado Profile.
 
+Este componente deve retornar uma foto sua, com o seu nome e uma mini bio (2 a 3 frases).
+
+O arquivo index.tsx deve ter um título (App criado para a disciplina Programação para Dispositivos Móveis) e o seu Profile.
+
+A entrega deve ser feita anexando 2 links:
+seu repositório no github
+seu app no Expo (over-the-air updates)
+OBS: NÃO é para anexar um arquivo texto com as URLs, é para anexar diretamente as duas URLs.
+
+*/
